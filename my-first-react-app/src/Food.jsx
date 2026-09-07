fd2303501ef8cf26cb803986c03dbd088773bef4
Lotta <lotsulotsu@gmail.com>
2026-09-07 15:00:10 +0300

    added a new component

---
+++ my-first-react-app/src/Food.jsx
@@ -0,0 +1,10 @@
+function Food() {
+  return (
+    <div>
+      <h2>My favorite food</h2>
+      <p>It is just food</p>
+    </div>
+  );
+}
+
+export default Food;
--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Food from "./Food.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <Food />
   </StrictMode>,
-)
+);
